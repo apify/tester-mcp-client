fb751615d73120f970ab433eae724ca2f9721ae5
Lukas Celnar <lukas.celnar@apify.com>
2025-04-24 17:01:56 +0200

    refactor: remove state from record name

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 
 import { createClient } from './clientFactory.js';
-import { BASIC_INFORMATION, CONVERSATION_STATE_RECORD_NAME, Event } from './const.js';
+import { BASIC_INFORMATION, CONVERSATION_RECORD_NAME, Event } from './const.js';
 import { ConversationManager } from './conversationManager.js';
 import { processInput, getChargeForTokens } from './input.js';
 import { log } from './logger.js';
@@ -126,7 +126,7 @@ const publicPath = path.join(path.dirname(filename), 'public');
 const publicUrl = ACTOR_IS_AT_HOME ? HOST : `${HOST}:${PORT}`;
 app.use(express.static(publicPath));
 
-const persistedConversation = (await Actor.getValue<MessageParam[]>(CONVERSATION_STATE_RECORD_NAME)) ?? [];
+const persistedConversation = (await Actor.getValue<MessageParam[]>(CONVERSATION_RECORD_NAME)) ?? [];
 
 const conversationManager = new ConversationManager(
     input.systemPrompt,
@@ -141,7 +141,7 @@ const conversationManager = new ConversationManager(
 
 Actor.on('persistState', async () => {
     log.debug(`Persisting conversation.`);
-    await Actor.setValue(CONVERSATION_STATE_RECORD_NAME, conversationManager.getConversation());
+    await Actor.setValue(CONVERSATION_RECORD_NAME, conversationManager.getConversation());
 });
 
 // Only one browser client can be connected at a time
